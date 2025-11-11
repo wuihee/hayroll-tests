30d4327e5a5de13c88d67bfc460d387267ed564b
wuihee <wuihee@gmail.com>
2025-11-10 22:32:02 -0800

    feat: simple entrypoint to test runner

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import json
+from dataclasses import asdict
+
 from src import run_tests
 
-print(run_tests())
+test_results = run_tests()
+with open("test_results.json", "w") as file:
+    results_asdict = [asdict(result) for result in test_results]
+    json.dump(results_asdict, file, indent=2)
